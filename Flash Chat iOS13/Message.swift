4c98eee061a8807c0a778eb921a34fc0aff1e648
Artie <artiepolanco@gmail.com>
2020-06-03 13:28:16 -0400

    Created a model for messages

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Arturo Polanco on 6/3/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
